e34be56ece910a0fadc0e4536d83fa96df183b8e
Darshan <darshan.kb@outlook.com>
2023-06-04 13:41:39 +0530

    CountdownService added which executes every second

---
+++ src/main/java/com/spin/game/service/CountdownService.java
@@ -0,0 +1,20 @@
+package com.spin.game.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CountdownService {
+
+    private final int countdown;
+    private int varcount;
+    public CountdownService(@Value("${countdown}") int countdown){
+        this.countdown = countdown;
+        varcount = countdown;
+    }
+    @Scheduled(fixedRate = 1000)
+    public void countdownStart(){
+
+    }
+}
