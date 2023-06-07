eeeb6ed8cacaaeafb92bc6a213205b2056a684fa
Darshan <darshan.kb@outlook.com>
2023-06-07 14:30:56 +0530

    Create RegistrationController with registrationService and RegistrationRequest model

---
+++ src/main/java/com/spin/game/service/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.spin.game.service;
+
+import com.spin.game.model.RegistrationRequest;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request){
+        return "works";
+    }
+}
