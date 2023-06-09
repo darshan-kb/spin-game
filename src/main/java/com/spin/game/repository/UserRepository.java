087d92d67297086fb0ee7d1784bfebb0d13b9080
Darshan <darshan.kb@outlook.com>
2023-06-09 05:35:48 +0530

    Create confirmToken which will confirm only if the token is not expired and user has not confirmed its token

--- src/main/java/com/spin/game/repository/UserRepository.java
+++ src/main/java/com/spin/game/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.spin.game.repository;
 
 import com.spin.game.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.enabled=TRUE WHERE u.email=?1")
+    int enableUser(String email);
 }
--- src/main/java/com/spin/game/service/RegistrationService.java
+++ src/main/java/com/spin/game/service/RegistrationService.java
@@ -1,14 +1,19 @@
 package com.spin.game.service;
 
+import com.spin.game.entities.ConfirmationToken;
 import com.spin.game.entities.User;
 import com.spin.game.model.RegistrationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
-    SignUpService signUpService;
+    private final SignUpService signUpService;
+    private final ConfirmationTokenService confirmationTokenService;
     public String register(RegistrationRequest request){
 
         String token = signUpService.signUpUser(
@@ -21,6 +26,26 @@ public class RegistrationService {
                 )
         );
         return token;
+    }
+
+    @Transactional
+    public String confirmToken(String token){
+        ConfirmationToken confirmationToken = confirmationTokenService.getToken(token)
+                .orElseThrow(()->new IllegalStateException("token not found"));
+
+        if(confirmationToken.getConfirmedAt()!=null){
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if(expiredAt.isBefore(LocalDateTime.now())){
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+
+        return signUpService.enableUser(confirmationToken.getUser().getEmail())==2 ? "confirmed" : "couldn't register";
     }
 
 
